c19e90b38c428fe52c292e3f1ddfb72adf811e62
pankajpatel19 <pankajpatelinter@gmail.com>
2026-09-13 10:53:11 +0530

    feat : setup llm client

--- src/llmClient.ts
+++ src/llmClient.ts
@@ -1,7 +1,23 @@
+import { OpenAI } from "openai/client";
 import type { LLMconfig } from "./types.js";
 
-
-export function LLM (config: LLMconfig,prompt: string):Promise<string> {
-    // Implementation for LLM client
-    throw new Error("LLM function is not implemented yet.");
+export async function LLM(config: LLMconfig, prompt: string): Promise<string> {
+  const client = new OpenAI({
+    apiKey: config.apiKey,
+  });
+  const res = await client.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: config.model,
+    temperature: config.temperature,
+    max_tokens: config.maxTokens,
+  });
+  return (
+    res.choices[0]?.message?.content?.toString() ??
+    Promise.reject("No response from LLM")
+  );
 }
